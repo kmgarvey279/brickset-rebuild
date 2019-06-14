b15f2e7a61fe84a793937e9d50576f1f515fc2e3
Kevin Garvey <kmgarvey279@gmail.com>
2019-06-14 16:25:24 -0700

    add nav bar and dummy links to main page template

--- rebuild/src/app/app.component.ts
+++ rebuild/src/app/app.component.ts
@@ -7,13 +7,13 @@ import { Set } from './models/set.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  currentFocus: string = 'Brickset Homepage';
+  currentFocus: string = "";
   selectedSet = null;
 
   masterSetList: Set[] = [
     new Set("Test Pirate Ship", 2711, "Normal", "Action/Adventure", "Pirates", "Zombie Pirates", 2019, "Zombie Pirate Ship Adventure", 502, 60.00, "7-12", "Box", "Retail"),
     new Set("Test Spaceship", 3322, "Other", "Action/Adventure", "Space", "Space Truckers", 2018, "Space Trucker Ship Adventure", 438, 45.00, "7-12", "Box", "Retail"),
-    new Set("Test Cafe", 213, "Normal", "Present Day", "Creator Expert", "Modular Buildings", 2017, "City Cafe Modular Expert", 2302, 160.00, "12+", "Box", "Exclusive")
+    new Set("Test Cafe", 2913, "Normal", "Present Day", "Creator Expert", "Modular Buildings", 2017, "City Cafe Modular Expert", 2302, 160.00, "12+", "Box", "Exclusive")
   ];
   addSet(newSet: Set) {
     this.masterSetList.push(newSet);
