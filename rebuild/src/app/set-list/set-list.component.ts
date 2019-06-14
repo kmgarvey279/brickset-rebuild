33db0d81e44944ce3905e3a2f2b59d8dbc876712
Kevin Garvey <kmgarvey279@gmail.com>
2019-06-14 10:52:32 -0700

    add set-list component content

--- rebuild/src/app/set-list/set-list.component.ts
+++ rebuild/src/app/set-list/set-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Set } from '../models/set.model';
 
 @Component({
   selector: 'app-set-list',
   templateUrl: './set-list.component.html',
   styleUrls: ['./set-list.component.css']
 })
-export class SetListComponent implements OnInit {
+export class SetListComponent {
+  @Input() childSetList: Set[];
+  @Output() clickSender = new EventEmitter();
+  filterByTheme: string = "displayNone";
+  filterByYear: string = "displayNone";
+  filterByCategory: string = "displayNone";
 
-  constructor() { }
-
-  ngOnInit() {
+  onChange(optionFromMenu) {
+    this.filterByTheme = optionFromMenu;
+    this.filterByYear = optionFromMenu;
+    this.filterByCategory = optionFromMenu;
   }
 
 }
